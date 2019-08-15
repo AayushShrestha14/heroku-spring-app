72621081cc33dce5e2fd83d7ca983f1e3ccb1c49
dixanta <dixanta@gmail.com>
2019-08-15 12:40:27 +0545

    first commit directly to heroku

---
+++ src/main/java/com/cibt/web/controller/HomeController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ *
+ * @author apple
+ */
+
+@Controller
+@RequestMapping(value = {"/"})
+public class HomeController{
+
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("name", "Creators Institute");
+        model.addAttribute("courses", 
+                new String[]{"Java","Python","PHP","ASP.net"});
+                
+        return "home/index";
+    }
+   
+}
